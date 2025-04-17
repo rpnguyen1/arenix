d2bc0eee3bc927bd7e81f8780a397f99d7b6b1e2
Richard Nguyen <nguyenprichard@gmail.com>
2025-04-16 23:36:45 -0700

    Updated metadata and about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,9 +3,18 @@ import React from "react";
 // import "./index.css";
 
 const Hello = () => (
-    <div>
-        what
+    <section id="about">
+    <h2>About Us</h2>
+    <p>
+    Arenix - /ɑɹ.ɛn.nɪks/ "are-ehn-nicks". 
+
+
+      Coming soon!
+    </p>
+    <div className="social-links">
+      <a href="https://www.instagram.com/arenixart/" target="_blank" rel="noopener noreferrer">instagram</a>
     </div>
+  </section>
 );
 
 export default Hello;
